f46ef61960a04cc934973aa1faa0d68b111ae89d
avantika2503 <avantiika.yadav@gmail.com>
2025-12-04 23:42:47 +0530

    init collector service

---
+++ collector-service/src/main/java/com/example/collector_service/CollectorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.collector_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectorServiceApplication.class, args);
+	}
+
+}
